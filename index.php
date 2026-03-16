0c0a4aa5c001e03cfb0c07793efd3011d49dde46
DavidARivera001 <david.rivera@galileo.edu>
2026-03-16 01:16:25 +0000

    agregamos codigo e imagen

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+ 
+ echo "Hola, mundo!, Soy David Andrés ";
+
+echo "<img src = '/proyecto_php/my_photo_like_cyberpunk_style.jpg' alt = 'Foto de David Andrés' width = '400' height = '400'>";
+
+ ?>
